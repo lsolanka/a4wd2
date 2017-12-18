98dc03a8d957c602acff26ea6d64c8c1f8d7e6e2
Lukas Solanka <lsolanka@gmail.com>
2017-12-18 23:22:50 +0000

    improve docs

--- src/sensor_reader/include/sensor_reader/sensor_reader.hpp
+++ src/sensor_reader/include/sensor_reader/sensor_reader.hpp
@@ -12,6 +12,14 @@
 namespace a4wd2::sensor_reader
 {
 
+/** Read sensor data and distribute the data to sensor subscribers registered
+ * with the add_sensor method.
+ *
+ * It is possible to subscribe any number of subscribers.
+ *
+ * It is currently not possible to unsubscribe an already subscribed sensor
+ * subscriber.
+ */
 class sensor_reader
 {
   public:
@@ -23,6 +31,20 @@ class sensor_reader
     {
     }
 
+    /** Register a new sensor subscriber.
+     *
+     * This method creates a new instance of the sensor_t class and forwards
+     * all the arguments to the constructor of the instance.
+     *
+     * The class of type sensor_t must inherit from
+     * a4wd2::sensor_reader::sensor and must define a static const string
+     * member called ID, which return the classification ID for the sensor
+     * data.
+     *
+     * It is possible to subscribe more sensors with the same classifier ID.
+     * The parsing will happen in the order of subscription (i.e. call to this
+     * method).
+     */
     template<typename sensor_t, typename... Args>
     void add_sensor(Args&&... args)
     {
@@ -31,6 +53,12 @@ class sensor_reader
                 args...));
     }
 
+    /** Read all data line-by-line, until the end of stream is reached or the
+     * stream is no longer good().
+     *
+     * All parsed data is passed for parsing to the registered sensor
+     * subscribers.
+     */
     void read_all();
 
   private:
